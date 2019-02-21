aefaebee8670844197261423ded9dfcc135cbecd
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-02-21 18:12:52 -0300

    Adding hospede api

---
+++ lodge-hospede-api/src/main/java/br/com/lodge/hospedeapi/LodgeHospedeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.lodge.hospedeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LodgeHospedeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LodgeHospedeApiApplication.class, args);
+	}
+
+}
