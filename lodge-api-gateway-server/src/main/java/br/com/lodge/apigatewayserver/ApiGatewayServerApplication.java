bc4d125ac9dd416e1e98fc63b83b43b3c5a70427
Thiago Henrique <thiagonascimento95@hotmail.com>
2019-02-23 17:53:10 -0300

    Evoluating consumo api

---
+++ lodge-api-gateway-server/src/main/java/br/com/lodge/apigatewayserver/ApiGatewayServerApplication.java
@@ -0,0 +1,24 @@
+package br.com.lodge.apigatewayserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ApiGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServerApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+	}
+}
